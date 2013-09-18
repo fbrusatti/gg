7beda795555b2fefe8bfaea58f06d8df63db881c
Rodrigo Arino <rodrigoarino@gmail.com>
2013-09-18 18:00:58 -0300

    modify order routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Gg::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
   root to: 'landing#index'
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
   ActiveAdmin.routes(self)
 
