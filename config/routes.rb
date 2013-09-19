f9567faa9dcfc7584b8711cd2c26344044d4a5f4
edu <edu.depetris.00@gmail.com>
2013-09-19 19:44:46 -0300

    Place the sign in as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Gg::Application.routes.draw do
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
+  get '/landing_page', to: 'landing#index'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
