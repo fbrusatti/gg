678593a0e6f47cd3b03846a1b96f60536c283c3c
Agustin <agvarea@gmail.com>
2013-10-01 12:53:57 -0300

    WiP on add customers

---
+++ spec/factories/customers.rb
@@ -0,0 +1,16 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :customer do
+    dni "MyString"
+    name "MyString"
+    surname "MyString"
+    mobile_phone "MyString"
+    phone "MyString"
+    email "MyString"
+    registered_name "MyString"
+    cuit_cuil "MyString"
+    gross_income "MyString"
+    address "MyString"
+  end
+end
