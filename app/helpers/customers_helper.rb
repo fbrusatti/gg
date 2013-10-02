5dd261219e089babe9632754fa7d8a4dbea80336
Agustin <agvarea@gmail.com>
2013-10-02 11:22:32 -0300

    Add file chenged

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -3,7 +3,7 @@ module CustomersHelper
   TYPE_PERSON     = %W{customer supplier}
   TYPE_IVA        = %w{non_registered_taxpayer tax_exempt monotributista registered_taxpayer}
 
-  def type_iva
+  def type_iva_meth
     CustomersHelper::TYPE_IVA.map { |i| [t("customers.type_iva.#{i}"), i] } 
   end
 end
