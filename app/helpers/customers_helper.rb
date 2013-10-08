a8d286c05d9c074cabf609039e5ada03acc14f40
Agustin <agvarea@gmail.com>
2013-10-08 12:16:21 -0300

    Remove type_person and remove translation on type_iva

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,9 +1,8 @@
 module CustomersHelper
   
-  TYPE_PERSON     = %W{customer supplier}
   TYPE_IVA        = %w{non_registered_taxpayer tax_exempt monotributista registered_taxpayer}
 
   def type_iva_meth
-    CustomersHelper::TYPE_IVA.map { |i| [t("customers.type_iva.#{i}"), i] } 
+    CustomersHelper::TYPE_IVA.map { |i| [t("customers.type_iva.#{i}"), t("customers.type_iva.#{i}")] } 
   end
 end
--- app/helpers/suppliers_helper.rb
+++ app/helpers/suppliers_helper.rb
@@ -1,10 +1,9 @@
 module SuppliersHelper
 
-  TYPE_PERSON     = %W{customer supplier}
   TYPE_IVA        = %w{non_registered_taxpayer tax_exempt monotributista registered_taxpayer}
 
   def type_iva_meth
-    SuppliersHelper::TYPE_IVA.map { |i| [t("suppliers.type_iva.#{i}"), i] } 
+    SuppliersHelper::TYPE_IVA.map  { |i| [t("suppliers.type_iva.#{i}"), t("suppliers.type_iva.#{i}")] }
   end
 
 end
