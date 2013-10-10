408101ab44a7043c1fd364019fcece91949d0941
Agustin <agvarea@gmail.com>
2013-10-10 12:34:30 -0300

    Add category on activeAdmin

---
+++ app/models/category.rb
@@ -0,0 +1,11 @@
+class Category < ActiveRecord::Base
+  
+  # == Accesors
+  attr_accessible :name, :referrer_id
+
+  # == Associations
+  belongs_to :referrer, :class_name => 'Category'
+  has_many :referrals, :foreign_key => :referrer_id, 
+                       :class_name  => 'Category'
+
+end
