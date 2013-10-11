07640163e55e857a5c1fc1a2c8b8a05b8545d604
Agustin <agvarea@gmail.com>
2013-10-11 10:41:50 -0300

    Add category on products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ActiveRecord::Base
    # == Accesors
   attr_accessible :active, :code, :cost_price, :description, :gondola, 
                   :list_price_one, :list_price_three, :list_price_two, 
-                  :minimun_stock, :shelf, :stock               
+                  :minimun_stock, :shelf, :stock, :category_id         
+
+  # == Associations
+  has_many :categories
 
 end
