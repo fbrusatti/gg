d909c4bd72f0095cc3ab7e097e19695786bc5e8c
Agustin <agvarea@gmail.com>
2013-10-21 23:16:53 -0300

    Add category

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,5 +1,13 @@
 ActiveAdmin.register Category do
 
+  index do
+    column :id
+    column :name
+    column :referrer_id
+    column :created_at
+    default_actions
+  end
+
   filter :referrer_id
   filter :name
   
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,17 @@
 class CategoriesController < ApplicationController
+
+  respond_to :html
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params[:category])
+    if @category.save
+      flash[:success] = t('flash.category', message: t('flash.created'))
+    end
+    redirect_to new_product_path
+  end
+
 end
