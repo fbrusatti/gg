438975cc8792d3ab5990ce3dddebdd0cd87b896a
Rodrigo Arino <rodrigoarino@gmail.com>
2013-10-29 19:19:22 -0300

    add datatimepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-datetimepicker.min
 //= require dataTables/jquery.dataTables
 //= require jquery.tokeninput
 //= require_tree .
