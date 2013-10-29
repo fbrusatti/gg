5322cb9ce286f20531cd896ac1e52f6d364e862d
Rodrigo Arino <rodrigoarino@gmail.com>
2013-10-29 17:41:54 -0300

    add restriccion to select

--- app/assets/javascripts/banks.js
+++ app/assets/javascripts/banks.js
@@ -9,12 +9,37 @@
 
   $('.btn-modal-submit').click(function(){
     $('#bank-modal').modal('hide');
-    $('#bank_name')
-      .append($("<option></option>")
-      .attr("value",$('#new-name-bank').val().toUpperCase())
-      .text($('#new-name-bank').val().toUpperCase())
-    );
-     console.log($('#new-name-bank').val());
+    var sel = document.getElementById('bank_name');
+    var name = $('#new-name-bank').val().toUpperCase();
+    if (not_exists_name(sel, name)){
+      $('#bank_name')
+        .append($("<option></option>")
+        .attr("value",name)
+        .text(name)
+      );
+    }
+    selectItemByValue(sel, name )
+    $('#new-name-bank').val('');
   });
 
+  function selectItemByValue(elmnt, value){
+
+    for(var i=0; i < elmnt.options.length; i++)
+    {
+      if(elmnt.options[i].text == value)
+        elmnt.selectedIndex = i;
+    }
+  }
+
+  function not_exists_name(elmnt, value){
+
+    var aReturn = true
+
+    for(var i=0; i < elmnt.options.length; i++)
+    {
+      if(elmnt.options[i].text == value)
+        aReturn = false;
+    }
+    return aReturn;
+  }
 });
