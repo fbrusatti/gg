b4208d1980f4a9d1111b9b96de8858657bc5fdde
Agustin <agvarea@gmail.com>
2013-12-03 15:06:16 -0300

    Add document id to check

--- app/models/documents/document.rb
+++ app/models/documents/document.rb
@@ -4,7 +4,7 @@ class Document < ActiveRecord::Base
   attr_accessible :type, :number, :recharge, :amount, :balance, :state,
                   :expiration_date, :payment_condition, :active, :annul,
                   :creation_state, :customer_tokens, :amount_cash
-
+  
   attr_reader :customer_tokens
 
   # == Associations
