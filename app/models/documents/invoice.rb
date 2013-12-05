ee3ce109c6aa735e0d3250761fef7977b5b9a966
edu <edu.depetris.00@gmail.com>
2013-12-05 15:32:42 -0300

    Add search customer with TokenInput

--- app/models/documents/invoice.rb
+++ app/models/documents/invoice.rb
@@ -3,11 +3,15 @@ class Invoice < Document
   after_save :descount_amount, if: Proc.new { |i| i.creation_state == 'finish' }
 
   # == Accesors
-  attr_accessible :product_tokens
-  attr_reader :product_tokens
+  attr_accessible :product_tokens, :customer_tokens
+  attr_reader :product_tokens, :customer_tokens
 
   def product_tokens=(id)
     self.product_id = id
+  end
+
+  def customer_tokens=(id)
+    self.customer_id = id
   end
 
   private
