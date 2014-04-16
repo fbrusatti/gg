291b78115ee6accc7d07089cc0ead61bdd69ba8e
Agustin <agvarea@gmail.com>
2014-04-16 19:03:00 -0300

    Add annul field in card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ActiveRecord::Base
 
 	# == Accessors
-  attr_accessible :amount, :code, :expirate_at, :name, :number, :type_card
+  attr_accessible :amount, :code, :expirate_at, :name, :number, :type_card, :annul
 
   # == Validations
   validates_presence_of :type_card, :name, :amount
