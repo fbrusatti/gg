df2811782f3edca58939d40c44738f89be913e52
Agustin <agvarea@gmail.com>
2014-04-16 19:05:31 -0300

    Add field annul to accessors in check and item models

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
 
   # == Accesors
   attr_accessible :amount, :price_cost, :price_sale, :price_vat, :price_descount,
-                  :document_id, :product_id, :total_price
+                  :document_id, :product_id, :total_price, :annul
 
   # == Associations
   belongs_to :document
