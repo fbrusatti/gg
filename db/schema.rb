d6ad4f93e77a938e0c6ddee7c0b8e69bf2949694
Agustin <agvarea@gmail.com>
2014-04-16 11:36:20 -0300

    Add annul field to Card model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140414144752) do
+ActiveRecord::Schema.define(:version => 20140416142844) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -65,8 +65,9 @@ ActiveRecord::Schema.define(:version => 20140414144752) do
     t.string   "name"
     t.string   "code"
     t.date     "expirate_at"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
+    t.boolean  "annul",       :default => false
   end
 
   create_table "categories", :force => true do |t|
