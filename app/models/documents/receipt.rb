8bbf0e9378a7fa9baa5e17561a38bbe251b76063
Agustin <agvarea@gmail.com>
2014-04-21 14:55:40 -0300

    Complete receipt

--- app/models/documents/receipt.rb
+++ app/models/documents/receipt.rb
@@ -5,31 +5,35 @@ class Receipt < Document
   has_many :invoices, :through => :payments
 
   # == Callbacks
-  after_save :discount_invoices
+  before_save :calculate_amounts, :discount_invoices
+
+
+  def calculate_amounts
+    cards_array = self.cards
+    cards_array.map { |card| self.amount = self.amount + card.amount }  
+    checks_array = self.checks
+    checks_array.map { |check| self.amount = self.amount + check.amount }
+    self.amount = self.amount + self.amount_cash
+    self.balance = self.amount
+  end
 
   def discount_invoices
-    invoice_id = self.invoices
-    invoice_id.map { |invoice| verify_conditions(invoice) }
+    invoices_array = self.invoices
+    invoices_array.map { |invoice| verify_conditions(invoice) }
   end
 
   def verify_conditions(invoice)
-    if (invoice.balance != 0) 
-      new_balance = (invoice.balance - self.amount)
-      if (new_balance < 0) 
-        invoice.balance = 0;
+    if (invoice.balance > 0) 
+      new_balance = (invoice.balance - self.balance)
+      if (new_balance <= 0) 
+        invoice.balance = 0
+        invoice.annul = true
         invoice.save
-        @new_receipt = Receipt.new()
-        @new_receipt = self
-        @new_receipt.balance = new_balance
-        @new_receipt.save
+        self.balance = new_balance * (-1)
       elsif (new_balance > 0) 
         invoice.balance = new_balance
         invoice.save
-      else 
-        # This condition maybe will can merge with last condition, but (new balance >= 0)
-        invoice.balance = 0
-        #Sacar de la cuenta corriente
-        invoice.save
+        self.balance = new_balance
       end
     end 
   end
